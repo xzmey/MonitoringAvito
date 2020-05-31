efaf4b56982c2e0e7006fe3f77bfbd37f7779bca
xzmey <alb281195@mail.ru>
2020-05-31 18:06:03 +0400

    конфиг для удобства

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+define('VK_KEY',"4dec5adac64862cecd0ebf2cef7e2aa01bb1e86b42abf2df5731c299d7d1204b80173798e8458dc7243b1");
+define('ACCESS_KEY',"b996e079");
+define('VERSION',"5.0");
+define('URL',"http://d8891ca148d0.ngrok.io");
